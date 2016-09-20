654eb9e28ad48841231cd376daecae1a3a03e559
willardshikami <willardshikami@gmail.com>
2016-09-20 08:28:55 +0300

    added tab layout in the main java file

--- app/src/main/java/com/example/willardpc/ukulimainfo/MainActivity.java
+++ app/src/main/java/com/example/willardpc/ukulimainfo/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.willardpc.ukulimainfo;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.design.widget.TabLayout;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -22,6 +23,12 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+            TabLayout tabs = (TabLayout) findViewById(R.id.tabs);{
+            tabs.addTab(tabs.newTab().setText("Coffee"));
+            tabs.addTab(tabs.newTab().setText("Tea"));
+            tabs.addTab(tabs.newTab().setText("Maize"));
+    }
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
